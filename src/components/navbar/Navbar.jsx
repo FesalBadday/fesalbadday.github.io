1a6a082002ea579f72815bae5f3f6d8ca5ab90e1
FesalBadday <fesal.n04@hotmail.com>
2021-12-28 21:53:42 -0700

    removed email

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,12 @@
 import Logo from '../../Logo.svg'
 import './navbar.scss'
-import { Mail } from '@material-ui/icons'
 
 export default function Navbar({ menuOpen, setMenuOpen }) {
   return (
     <div className={'navbar ' + (menuOpen && 'active')}>
       <div className="wrapper">
         <div className="logo">
-          <img src={Logo} alt="FB Logo" />
-          <div className="email">
-            <Mail className='icon' />
-            <a href="mailto:fesal.n04@hotmail.com">fesal.n04@hotmail.com</a>
-          </div>
+          <a href="#home"><img src={Logo} alt="FB Logo" /></a>
         </div>
         <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
           <span className='line1'></span>
