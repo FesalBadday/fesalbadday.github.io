24e161272195973167c05d8f288aed1e7a4a7873
FesalBadday <fesal.n04@hotmail.com>
2021-12-28 22:20:03 -0700

    initial commit

---
+++ src/projects.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    id: "1",
+    title: "Blackjack Gmae",
+    desc: "blackjack game",
+    img: "./assets/projects/blackjack.webp",
+    link: "https://blackjack265.herokuapp.com"
+  },
+  {
+    id: "2",
+    title: "Blackjack Gmae",
+    desc: "blackjack game",
+    img: "./assets/projects/blackjack.webp",
+    link: "https://blackjack265.herokuapp.com"
+  },
+  {
+    id: "3",
+    title: "Blackjack Gmae",
+    desc: "blackjack game",
+    img: "./assets/projects/blackjack.webp",
+    link: "https://blackjack265.herokuapp.com"
+  },
+]
