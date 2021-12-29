8065097e01e794f1b55acae02fa58bac707a1827
FesalBadday <fesal.n04@hotmail.com>
2021-12-28 21:55:40 -0700

    added projects section

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,48 @@
 import './projects.scss'
+import { useState } from "react"
+import { data } from "../../projects"
+import { GrPrevious, GrNext } from 'react-icons/gr'
 
 export default function Projects() {
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const handleClick = (direction) => {
+    direction === 'previous'
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0)
+  }
+
   return (
     <div className='projects' id='projects'>
-      
+      <div className="slideNum">
+        {`${currentSlide + 1} / ${data.length}`}
+      </div>
+
+      <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="arrowContainer">
+                    <GrPrevious className='arrow' />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>
+                    {d.desc}
+                  </p>
+                  <span>Projects</span>
+                </div>
+              </div>
+              <div className="right">
+                <img src={d.img} alt="" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <GrPrevious className='arrow previous' onClick={() => handleClick("previous")} />
+      <GrNext className='arrow next' onClick={() => handleClick()} />
     </div>
   )
 }
