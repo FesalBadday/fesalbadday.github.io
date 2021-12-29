8bfd1c358d49dc174bca157b05fdccc615b3ba7f
FesalBadday <fesal.n04@hotmail.com>
2021-12-28 21:52:04 -0700

    added about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,19 @@
+import Tools from '../tools/Tools'
 import './about.scss'
 
 export default function About() {
   return (
     <div className='about' id='about'>
-      
+      <div className='info'>
+        {/* <p>
+          I have graduated from SAIT (Southern Alberta Institute Technology) as a Junior Web Developer,
+          I am motivated in designing and developing websites and then learn how to apply them in a business-oriented environment.
+          I am excited to start my career as a Web Developer
+        </p> */}
+      </div>
+      <div className="skills">
+        <Tools />
+      </div>
     </div>
   )
 }
