60153c4e3468479156d95fb6890eb77b48cc173d
FesalBadday <fesal.n04@hotmail.com>
2021-12-29 12:53:03 -0700

    tweaks

--- src/projects.js
+++ src/projects.js
@@ -1,23 +1,44 @@
 export const data = [
   {
     id: "1",
-    title: "Blackjack Gmae",
-    desc: "blackjack game",
+    title: "Blackjack Game",
+    desc: "blackjack game with leaderboard.",
     img: "./assets/projects/blackjack.webp",
     link: "https://blackjack265.herokuapp.com"
   },
   {
     id: "2",
-    title: "Blackjack Gmae",
-    desc: "blackjack game",
-    img: "./assets/projects/blackjack.webp",
-    link: "https://blackjack265.herokuapp.com"
+    title: "Instagram Integration",
+    desc: "Integrated Instagram content into a website.",
+    img: "./assets/projects/instagram.webp",
+    link: "https://cpnt270-a3.netlify.app"
   },
   {
     id: "3",
-    title: "Blackjack Gmae",
-    desc: "blackjack game",
-    img: "./assets/projects/blackjack.webp",
-    link: "https://blackjack265.herokuapp.com"
+    title: "Team Cards",
+    desc: "Simple team cards page.",
+    img: "./assets/projects/cards.webp",
+    link: "https://fesal.me/roster-cards"
   },
+  {
+    id: "4",
+    title: "Fetching API data",
+    desc: "Fetching data asynchronously from a public API.",
+    img: "./assets/projects/fetch.webp",
+    link: "https://fesal.me/cpnt262-a3"
+  },
+  {
+    id: "5",
+    title: "Cars Gallery API",
+    desc: "Made my own API for photos of cars.",
+    img: "./assets/projects/api.webp",
+    link: "https://cpnt262-a5-fesal.herokuapp.com"
+  },
+  {
+    id: "6",
+    title: "Hero Section",
+    desc: "Landing page with a hero section.",
+    img: "./assets/projects/hero.webp",
+    link: "https://fesal.me/cpnt260-a4"
+  }
 ]
