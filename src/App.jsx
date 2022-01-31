cd11f83f0228fda4ebf7c3509f2e30d55ffff5e5
FesalBadday <fesal.n04@hotmail.com>
2022-01-30 23:55:17 -0700

    tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Projects from './components/projects/Projects'
 import Contact from './components/contact/Contact'
 import './app.scss'
 import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 /* fetch('https://api.countapi.xyz/update/fesal.me/portfolio?amount=1') */
 
@@ -14,20 +14,20 @@ function App() {
   const [menuOpen, setMenuOpen] = useState(false)
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="app">
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <div className="sections">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about*" element={<About />} />
-            <Route path="/projects*" element={<Projects />} />
-            <Route path="/contact*" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
