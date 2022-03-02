cf7333a36dc6f8071db90c17c4b6a431ead47b48
FesalBadday <fesal.n04@hotmail.com>
2022-03-02 01:44:26 -0700

    tweaks

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,10 +11,18 @@ export default function Navbar({ menuOpen, setMenuOpen }) {
         </div>
         <div className='nav'>
           <ul>
-            <li><Link to="/" data-text="&nbsp;Home">&nbsp;Home</Link></li>
-            <li><Link to="/skills" data-text="&nbsp;Skills">&nbsp;Skills</Link></li>
-            <li><Link to="/work" data-text="&nbsp;Work">&nbsp;Work</Link></li>
-            <li><Link to="/contact" data-text="&nbsp;Contact">&nbsp;Contact</Link></li>
+            <li>
+              <Link to="/" class="text" data-text="Home">Home</Link>
+            </li>
+            <li>
+              <Link to="/skills" class="text" data-text="Skills">Skills</Link>
+            </li>
+            <li>
+              <Link to="/work" class="text" data-text="Work">Work</Link>
+            </li>
+            <li>
+              <Link to="/contact" class="text" data-text="Contact">Contact</Link>
+            </li>
           </ul>
         </div>
         <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
