06910d738bf8914722057ad9b4e94796cb0a0592
FesalBadday <fesal.n04@hotmail.com>
2022-04-13 18:02:51 -0600

    tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './components/about/About'
 import Projects from './components/projects/Projects'
 import Contact from './components/contact/Contact'
 import Error from './components/404/404'
-import wtslog7 from 'react'
 import './app.scss'
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
@@ -14,10 +13,6 @@ fetch('https://api.countapi.xyz/update/fesal.me/portfolio?amount=1')
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false)
-
-  const wts = document.createElement('script'); wts.async = true;
-  wts.src = 'https://app.ardalio.com/log7.js'; document.head.appendChild(wts);
-  wts.onload = function () { wtslog7(2097325, 3); };
 
   return (
     <div className="app">
@@ -32,10 +27,9 @@ function App() {
           <Route path="*" element={<Error />} />
         </Routes>
       </div>
-      <span id="wts2097325"></span>
       <a href="https://www.web-stat.com">
-        <img src="https://app.ardalio.com/7/3/2097325.png"
-          alt="Web-Stat traffic analysis" /></a>
+<img alt="Web-Stat web statistics" src="https://app.ardalio.com/7/3/2097325.png" />
+</a>
     </div>
   );
 }
